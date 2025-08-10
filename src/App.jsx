d71edaf860f60aca547225a9c2a6c49aa45b53a9
Rahul Heer <rahulheer344@gmail.com>
2025-08-10 21:39:15 +0530

    project chnage to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,10 @@
-import { useEffect, useState } from 'react'
 import React from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './index.css'
-import { ThemeProvider } from './context/Theme'
-import ThemeBtn from './components/ThemeBtn'
-import Card from './components/card'
 
-function App() {
-  const [themeMode,setThemeMode]=useState("light");
-  const lightTheme=()=>{
-    setThemeMode("light");
-  }
-  const darkTheme=()=>{
-    setThemeMode("dark");
-  }
-  useEffect(()=>{
-    document.querySelector('html').classList.remove("dark","light")
-    document.querySelector('html').classList.add(themeMode)
-
-  },[themeMode])
-
+const App = () => {
   return (
-    <ThemeProvider value={{themeMode, lightTheme,darkTheme}}>
-      <div className='flex flex-wrap min-h-screen items-center'>
-        <div className='w-full'>
-          <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
-            <ThemeBtn/>
-          </div>
-          <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
-            <Card/>
-          </div>
-        </div>
-      </div>
-     
-    </ThemeProvider>
+    <div>
+
+    </div>
   )
 }
 
---
+++ src/context/index.js
@@ -0,0 +1,2 @@
+import React from 'react'
+export {TodoContext,Todoprovider,useTodo} from "./Todocontext"
