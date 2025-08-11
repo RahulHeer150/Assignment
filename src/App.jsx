3fae18ec0c316f0ce4ca6704ce3310942c265249
Rahul Heer <rahulheer344@gmail.com>
2025-08-11 22:48:53 +0530

    read

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,46 @@ import { Todoprovider } from './context'
 import TodoForm from './components/TodoForm'
 import TodoItem from './components/TodoItem'
 
-const App = () => {
-  const todos = [todos,setTodos] = React.useState([])
-  const addTodo=(todo)=>{
-    setTodos((prev)=>[{id:Date.now(),...todo},...prev])
-  }
-  const updateTodo=(id,todo)=>{
-    setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id===id ? todo:prevTodo)))
+function App() {
+  const [todos, setTodos] = useState([])
 
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
   }
-  const deleteTodo=(id)=>{
-    setTodos((prev)=>prev.filter((todo)=>todo.id !==id))
-  }
-  const toggleComplete=(id)=>{
-    setTodos((prev)=>prev.map((prevTodo)=>prevTodo===id ?{...prevTodo,completed:!prevTodo.completed}:prevTodo))
-  }
-  useEffect=(()=>{
-   const todoss= JSON.parse(localStorage.getItem("todos"))
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
+
     
-   if(todos && todos.length>0){
-    setTodos(todos)
-   }
+  }
 
-  },[])
-  useEffect(()=>{
-    localStorage.setItem("todos",JSON.stringify(todoss))
-  },[todoss])
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    //console.log(id);
+    setTodos((prev) => 
+    prev.map((prevTodo) => 
+      prevTodo.id === id ? { ...prevTodo, 
+        completed: !prevTodo.completed } : prevTodo))
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+  
 
   return (
-    <Todoprovider value={{todos,addTodo,updateTodo,deleteTodo,toggleTodo}}>
+    <Todoprovider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
